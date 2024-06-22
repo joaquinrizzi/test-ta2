42fff2850de3812fd320655a547aaa8483bd483b
Joaquin Soto <joaquinsotorizzi@gmail.com>
2024-06-22 17:35:34 +0200

    models creation and first migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_22_153255) do
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "address", null: false
+    t.string "phone_number"
+    t.string "category", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating", null: false
+    t.text "content", null: false
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+  end
+
+  add_foreign_key "reviews", "restaurants"
 end
